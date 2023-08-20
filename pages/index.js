1e8d500c552bceb8004a5a6cbf033ed9cef35159
Jai Aggarwal <jaiaggarwal60@gmail.com>
2023-08-20 19:03:41 +0530

    white bg

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { Footer, Navbar } from '../components';
 import { GetStarted1, GetStarted2, Hero, Insights, WhatsNew1,WhatsNew2, World } from '../sections';
 
 const Home = () => (
-  <div className="bg-primary-black overflow-hidden">
+  <div className="bg-white overflow-hidden">
     <Navbar />
 
     <div className="relative">
